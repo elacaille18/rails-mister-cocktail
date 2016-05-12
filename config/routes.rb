02f62041ae01d202a1ff6babee2a0fa4042e3d51
Eric Lacaille <elacaille0@gmail.com>
2016-05-12 18:43:58 +0200

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:create, :edit, :update, :destroy]
   end
+
+  root 'cocktails#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
